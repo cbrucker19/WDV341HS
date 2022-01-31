c0f24bd030a00fa7439ca677fdda089096837b7e
cbrucker19 <cyrus.j.brucker@gmail.com>
2022-01-31 13:11:55 -0600

    Initial commit

---
+++ homeWorkPage.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Cyrus' Brucker PHP Home Work</title>
+        <h1>Cyrus' Brucker PHP Home Work</h1>
+        <h2>Welcome to Cyrus' PHP Home Work! </h2>  
+    </head>
+    <body>
+        <h1>Welcome to my PHP WDV341 Homework page</h1>
+        <h3>Author: Cyrus Brucker </h3>
+    </body>
+</html>         

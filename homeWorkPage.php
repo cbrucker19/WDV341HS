06708177f92fb5c65ecc8d59ac9d6664a9657084
cbrucker19 <cyrus.j.brucker@gmail.com>
2022-02-25 17:17:43 -0600

    Selecting One Event HW
    Updating the homwork page, and adding the Selecting One event homework assignement

--- homeWorkPage.php
+++ homeWorkPage.php
@@ -14,5 +14,6 @@
         <p><a href="functionsAssignment.php">Function HomeWork</p>
         <p><a href="formHandling/WDVform.html">Form HomeWork</a></p>
         <p><a href="selectEvents.php">Selecting all events HomeWork</p>
+        <p><a href="selectOneEventHW.php">Select One event HomeWork</p>
     </body>
 </html>         
